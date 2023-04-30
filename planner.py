eac5f97c4e29ec844853727f3d6e88214947fe8d
Gustavo <theprodiggy@protonmail.com>
2023-04-30 19:32:15 -0300

    Port

--- planner.py
+++ planner.py
@@ -242,5 +242,4 @@ def parse_to_pdf(files, name):
 # dias_ate_OBF = (OBF_data - today).days # aqui retorna o numero de dias
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT'))
-    app.run(host='0.0.0.0', port=port)
+    app.run()
